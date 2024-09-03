29f0c008e7fe27004b7238d11d1b943c2ea71f7d
Aya Abd Elmoneim <114832629+AyaAbdElmoneim158@users.noreply.github.com>
2024-09-03 21:05:13 +0300

    Create 11.Find Maximum and Minimum Values of a List.cpp
    3/9/2024

---
+++ 11.Find Maximum and Minimum Values of a List.cpp
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/577a98a6ae28071780000989/solutions/cpp
+
+#include <vector>
+
+using namespace std;
+
+int min(vector < int > list) {
+
+  return * min_element(list.begin(), list.end());
+}
+
+int max(vector < int > list) {
+
+  return * max_element(list.begin(), list.end());
+}
