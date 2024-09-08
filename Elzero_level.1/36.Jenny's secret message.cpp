21150cfd0b921fdd29eb925c259ba2ca4580fffd
Aya Abd Elmoneim <114832629+AyaAbdElmoneim158@users.noreply.github.com>
2024-09-08 14:25:44 +0300

    Create 36.Jenny's secret message.cpp
    8/9/2024

---
+++ Elzero_level.1/36.Jenny's secret message.cpp
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/55225023e1be1ec8bc000390/solutions/cpp
+
+std::string greet(std::string name) 
+{
+  return "Hello, " + (name == "Johnny"? "my love":name ) + "!";
+}
