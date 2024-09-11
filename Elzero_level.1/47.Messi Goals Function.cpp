951e93eb8a1c9eeb75fbfe55913b2fdb640d0ce4
Aya Abd Elmoneim <114832629+AyaAbdElmoneim158@users.noreply.github.com>
2024-09-11 18:16:15 +0300

    Create 47.Messi Goals Function.cpp
    11/9/2024

---
+++ Elzero_level.1/47.Messi Goals Function.cpp
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4/solutions/cpp
+
+#define 🎯 (laLigaGoals + copaDelReyGoals + championsLeagueGoals)
+
+int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+  return 🎯;
+}
