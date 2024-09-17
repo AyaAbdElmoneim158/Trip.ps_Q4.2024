0b6453e4199fb40f3d530c611aecf6c075ab4ab8
Aya Abd Elmoneim <114832629+AyaAbdElmoneim158@users.noreply.github.com>
2024-09-17 18:02:44 +0300

    Create 60.Reversed Sequence.cpp
    17/9/2024

---
+++ Elzero_level.1/60.Reversed Sequence.cpp
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d/solutions/cpp
+
+std::vector < int > reverseSeq(int n) {
+  std::vector < int > result {};
+  for (int i = n; i > 0; i--) {
+    result.push_back(i);
+  }
+  return result;
+}
